617f98603de88b055668212a730f459c05533056
itizidon <dng21@binghamton.edu>
2020-03-09 14:49:29 -0400

    added upper and lower bands

--- app/actions/stock.js
+++ app/actions/stock.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { std } from 'mathjs';
 import type { Dispatch } from '../reducers/types';
 import { stockPrice, bollingerBands } from '../reducers/stock';
 
@@ -6,22 +7,32 @@ export function bollingerBandsCalc(data) {
   return async (dispatch: Dispatch) => {
     const days = Number(data.days);
     const dataWithBands = [];
-    console.log(data, 'THIS IS DATA');
+    const upperBand = [];
+    const lowerBand = [];
     for (let x = 20; x < days + 20; x += 1) {
       let sum = 0;
+      const last20prices = [];
       for (let y = 19; y >= 0; y -= 1) {
         sum += Number(data.arrData[x - y][1]);
+        last20prices.push(data.arrData[x - y][1]);
       }
-      console.log(sum);
+      const standardDeviation = std(last20prices, 'uncorrected');
+      upperBand.push([data.arrData[x][0], sum / 20 + standardDeviation * 2]);
+      lowerBand.push([data.arrData[x][0], sum / 20 - standardDeviation * 2]);
       dataWithBands.push([data.arrData[x][0], sum / 20]);
     }
-    console.log(dataWithBands);
     dispatch(
       stockPrice([
-        { label: 'Series 1', data: data.arrData.slice(20, days + 20) },{
+        { label: 'Series 1', data: data.arrData.slice(20, days + 20) },
+        {
           label: 'Simple Moving Average',
           data: dataWithBands
-        }
+        },
+        {
+          label: 'Upper Band',
+          data: upperBand
+        },
+        { label: 'Lower Band', data: lowerBand }
       ])
     );
   };
